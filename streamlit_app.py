21de01b94105c4f8d636e4f10a7a2b68fc3e036e
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-03 20:46:03 +0800

    Update streamlit_app.py
    Corrected the mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,4 +16,4 @@ streamlit.text("🍞 Hard-Boiled Free-Range Egg")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.dataframe(streamlit)
+streamlit.dataframe(my_fruit_list)
