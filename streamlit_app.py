920c943ad2f71b5bf3251ba599a616d1414a8aad
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-03 19:48:51 +0800

    Update streamlit_app.py
    Added additional information for breakfast meal

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My parents new Healthy Diner")
+
+streamlit.title("Omega 3 & Bluebery Oatmeal")
+
+streamlit.title("Kakle, Spanish & Rocket Smoothie")
+
+streamlit.title("Hard-Boiled Free-Range Egg")
