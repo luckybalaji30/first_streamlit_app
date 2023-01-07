8a46892f3d5d68e208677baa50a2238d2772f165
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-07 21:43:50 +0800

    Update streamlit_app.py
    Add function to receive fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,11 +63,20 @@ try:
 except URLError as e:
     streamlit.error()
     
-streamlit.stop()
 
-#snowflake settings
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+  
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
+  
+streamlit.stop()
 
 # try to fetch some values from the configuration
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
