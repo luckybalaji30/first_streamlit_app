f530f9838c06df62508d447a6061281bb0f05cbc
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-07 17:16:15 +0800

    Update streamlit_app.py
    Corrected the typo mistakes

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruitvice Fruit Advice !")
 #Get the choice from user
-fruit_choice=streamlis.text_input('What fruit would you like information about?', 'Kiwi')
+fruit_choice=streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 fruitvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
