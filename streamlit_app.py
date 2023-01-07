fa4c63a8629ab3e2cef600d59e44660962dc3392
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-07 20:24:50 +0800

    Update streamlit_app.py
    updated database name..

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ my_cur = my_cnx.cursor()
 #streamlit.text(my_data_row)
 
 # try to fetch some values from the tables.
-my_cur.execute("select * from demo_db.public.fruit_load_list")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains")
 streamlit.dataframe(my_data_rows)
