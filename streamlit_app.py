cbbb34323bb2469f0df6668973afef1e0e2e0bdb
luckybalaji30 <114144780+luckybalaji30@users.noreply.github.com>
2023-01-07 21:50:33 +0800

    Update streamlit_app.py
    added 3rd function..

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ try:
 except URLError as e:
     streamlit.error()
     
-
+# second part 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
@@ -76,6 +76,18 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
   
   
+# third part
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+      
+add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
 streamlit.stop()
 
 # try to fetch some values from the configuration
